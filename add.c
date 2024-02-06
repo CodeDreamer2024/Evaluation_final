c2157cd998ce8abd14e353f44cd98a64a1d0099c
Sanyukta <156066558+CodeDreamer2024@users.noreply.github.com>
2024-02-06 15:06:01 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, diff;
+    int num1, num2, mul;
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
@@ -9,9 +9,9 @@ int main() {
     printf("Enter the second number: ");
     scanf("%d", &num2);
     
-    diff = num1 - num2;
+    mul = num1 * num2;
     
-    printf("The difference of %d and %d is %d.", num1, num2, diff);
+    printf("The product of %d and %d is %d.", num1, num2, mul);
     
     return 0;
 }
